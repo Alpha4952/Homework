eca165190e9cf48f75bce170441ec10aa4e8c2a7
Alpheta <93063359+Alpha4952@users.noreply.github.com>
2024-03-11 08:28:21 +0700

    DId Milk2 (can't find milk1)

---
+++ Greedy/3. Milk2/Milk2.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long n, m, a[100001];
+
+bool sorter (long long a, long long b) {
+    return a > b;
+}
+
+int main () {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    freopen("Milk2.inp", "r", stdin);
+    freopen("Milk2.out", "w", stdout);
+    
+    cin >> n;
+    for (int i = 0; i < n; i++) cin >> a[i];
+    sort(a, a+n, sorter);
+
+    for (int i = 0; i < n; i++) {
+        if (a[i] - i == 0) break;
+        m += a[i]-i;
+    }
+
+    cout << m;
+}
