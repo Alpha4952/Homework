88d362d5f5365f65e31098f43ad66b7f6c2bca24
Alpheta <93063359+Alpha4952@users.noreply.github.com>
2024-03-11 08:05:28 +0700

    Did Dragon

---
+++ Greedy/1. Dragon/Dragon.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long n, s;
+pair <long long, long long> p[10001];
+
+bool sorter (pair <long long, long long> a, pair <long long, long long> b) {
+    return a.first < b.first;
+}
+
+int main () {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> s >> n;
+
+    for (int i = 0; i < n; i++) cin >> p[i].first >> p[i].second;
+
+    sort(p, p+n, sorter);
+
+    for (int i = 0; i < n; i++) {
+        if (s > p[i].first) s += p[i].second;
+        else {
+            cout << "NO";
+            return 0;
+        }
+    }
+    cout << "YES";
+}
