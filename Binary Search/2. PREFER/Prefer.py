27c040822825767214dbd16ff33442e9aa35d01e
Alpheta <93063359+Alpha4952@users.noreply.github.com>
2024-03-31 19:23:55 +0700

    Did smth idk

--- Binary Search/2. PREFER/Prefer.py
+++ Binary Search/2. PREFER/Prefer.py
@@ -6,20 +6,6 @@ n = int(input())
 audience = list(map(int, input().split()))
 audience.sort()
 
-def bisearch(target: int, array: list):
-    l = 0; r = len(array)
-    
-    while l < r:
-        mid = (r-l)//2 + l
-        
-        if array[mid] == target:
-            return True
-        elif array[mid] < target:
-            l = mid+1
-        else:
-            r = mid
-    return False
-
 p = int(input())
 for i in range(p):
     lb, rb = map(int, input().split())
