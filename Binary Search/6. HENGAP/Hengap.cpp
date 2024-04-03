9339060abc9befd122f235ccd9e1162f309b4378
Alpheta <93063359+Alpha4952@users.noreply.github.com>
2024-04-03 08:03:01 +0700

    First try b*tch

---
+++ Binary Search/6. HENGAP/Hengap.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long n, a[300001], r, res, mid;
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    freopen("Hengap.inp", "r", stdin);
+    freopen("Hengap.out", "w", stdout);
+
+    cin >> n >> r;
+    for (int i = 0; i < n; i++) cin >> a[i];
+    sort(a, a + n);
+
+    for (int i = 0; i < n; i++) res += n - (lower_bound(a, a+n, a[i] + r + 1) - a);
+    cout << res;
+}
