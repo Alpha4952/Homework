a3d1b502411b47baf029a08eb4ca9eeb43ee9111
Alpheta <93063359+Alpha4952@users.noreply.github.com>
2024-04-03 16:40:35 +0700

    Did Bai1 (while it's the 10th one)

---
+++ Binary Search/10. Bai1/Bai1.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long a, b, m, fs, ss, dif;
+
+int main () {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    freopen("Bai1.inp", "r", stdin);
+    freopen("Bai1.out", "w", stdout);
+    
+    cin >> a >> b;
+
+    m = (a+b)/2;
+    for (int i = a; i <= m; i++) fs += i;
+    for (int i = m+1; i <= b; i++) ss += i;
+
+    dif = abs(fs - ss);
+
+    while (m >= a && m < b) {
+        if (abs(fs - m - ss - m) < dif) {
+            m--;
+            dif = abs(fs - m - ss - m);
+        } else if (abs(fs + m + 1 - ss + m - 1) < dif) {
+            m++;
+            dif = abs(fs + m + 1 - ss + m - 1);
+        } else break;
+    }
+    cout << m;
+}
