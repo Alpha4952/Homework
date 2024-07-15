07fbb6865abf4ed2a6c7250fe0621465f5119968
Alpheta <93063359+Alpha4952@users.noreply.github.com>
2024-07-15 23:13:14 +0700

    Oh shesh

--- DP3/4. LineGame/LineGame.cpp
+++ DP3/4. LineGame/LineGame.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long n, a[1000003], F1[1000003], F2[1000003];
+long long n, a[1000003], f1[1000003], f2[1000003];
 
 int main() {
     ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
@@ -12,14 +12,14 @@ int main() {
 
     for (int i = 1; i <= n; i++) cin >> a[i];
 
-    F1[1] = a[1];
-    F2[1] = 0;
+    f1[1] = a[1];
+    f2[1] = 0;
 
     for (int i = 2; i <= n; i++) {
-        F1[i] = max(F1[i - 1], F2[i - 1] + a[i]);
-        F2[i] = max(F2[i - 1], F1[i - 1] - a[i]);
+        f1[i] = max(f1[i - 1], f2[i - 1] + a[i]);
+        f2[i] = max(f2[i - 1], f1[i - 1] - a[i]);
     }
-    
-    cout << max(F1[n], F2[n]);
+
+    cout << max(f1[n], f2[n]);
     return 0;
 }
